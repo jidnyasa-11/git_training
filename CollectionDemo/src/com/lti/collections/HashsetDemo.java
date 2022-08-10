8a137a0abb41bba743d2dda6620b8938325bf99b
jidnyasa-11 <jid.falak30@gmail.com>
2022-08-10 12:35:23 +0530

    Commit

---
+++ CollectionDemo/src/com/lti/collections/HashsetDemo.java
@@ -0,0 +1,13 @@
+package com.lti.collections;
+
+public class HashsetDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Set<String > hs= new HashSet<String>();
+		
+		
+
+	}
+
+}
